23cf90250afe8abd2e1f074a51ec92c8e6b7a8f9
SeeeRGo <seeergo@yahoo.com>
2017-05-01 10:32:38 +0400

    ToDoList & ToDo components

--- app/components/ToDoApp.jsx
+++ app/components/ToDoApp.jsx
@@ -1,10 +1,34 @@
 var React = require('React');
+var ToDoList = require('ToDoList');
 
 var ToDoApp = React.createClass({
+	getInitialState: function() {
+		return {
+			todos: [
+				{
+					id: 1,
+					text: 'Walk the dog'
+				},
+				{
+					id:2,
+					text: 'Clean the yard'
+				},
+				{
+					id:3,
+					text: 'Take a shit'
+				},
+				{
+					id:4,
+					text: 'Kill myself'
+				}
+			]
+		};
+	},
 	render: function() {
+		var {todos} = this.state;
 		return (
 			<div>
-				ToDoApp.jsx
+				<ToDoList todos={todos}/>
 			</div>
 			);
 	}
