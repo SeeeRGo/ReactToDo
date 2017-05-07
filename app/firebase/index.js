2ae7318293c8fecc07eeb1aa4408565aa100be63
SeeeRGo <seeergo@yahoo.com>
2017-05-07 23:57:40 +0400

    changes

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 try {
 	var config = {
-	    apiKey: "AIzaSyCZmh-4KJEWd80sw3QUKW9l9fSj1c362ek",
-	    authDomain: "todo-24d28.firebaseapp.com",
-	    databaseURL: "https://todo-24d28.firebaseio.com",
-	    projectId: "todo-24d28",
-	    storageBucket: "todo-24d28.appspot.com",
-	    messagingSenderId: "1032767596039"
+	    apiKey: process.env.API_KEY,
+	    authDomain: process.env.AUTH_DOMAIN,
+	    databaseURL: process.env.DATABASE_URL,
+	    projectId: process.env.PROJECT_ID,
+	    storageBucket: process.env.STORAGE_BUCKET,
+	    messagingSenderId: process.env.MESSAGING_SENDER_ID
   	};
 
   	firebase.initializeApp(config);
